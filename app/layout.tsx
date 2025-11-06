c9a8f393fd42242cfd24788b51c3f600c2bbf64d
Jack Stewart <100209426+jackstewart254@users.noreply.github.com>
2025-11-06 18:11:19 +0000

    changes - Abdullah: 1. Previous change also didn't work, new amendment

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           attribute="class"
           enableSystem
           disableTransitionOnChange
+          defaultTheme="light"
           value={{
             light: "light",
             dark: "dark",
