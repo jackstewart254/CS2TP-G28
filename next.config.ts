643f9c79fe69888f3cdc7748245812b7d481fe72
Jack Stewart <100209426+jackstewart254@users.noreply.github.com>
2025-11-06 20:01:45 +0000

    Changes: 1. altered the next.config.ts to reduce type checking

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Ignore ESLint errors during production build
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Allow production builds even with type errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
