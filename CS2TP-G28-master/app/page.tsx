90f383183df0bb40eead5de7438be99c5bb83c38
Jack Stewart <100209426+jackstewart254@users.noreply.github.com>
2025-12-01 12:05:48 +0000

    changes:

--- CS2TP-G28-master/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Landing from "./landing/content";
-
-export default function Home() {
-  return <Landing />;
-}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,32 @@
+// "use client";
+// import { Footer } from "@/components/footer";
+// import { NavigationBar } from "@/components/navigation";
+// import Products from "./content";
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <NavigationBar />
+
+//       <Products />;
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Home;
 "use client";
+
 import { Footer } from "@/components/footer";
 import { NavigationBar } from "@/components/navigation";
-import Products from "./content";
+import ProductsContent from "./content"; // renamed import
 
-const Home = () => {
+export default function ProductsPage() {
   return (
     <div>
       <NavigationBar />
-      <Products />;
+      <ProductsContent />
       <Footer />
     </div>
   );
-};
-
-export default Home;
+}
